36c23655d6ec07cb8076f9b35eb32efa36a7b627
NahuelBurdisso <nahuelburdisso@hotmail.com>
2022-07-11 21:36:45 -0300

    Update city background animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import "./Header.css";
 import citySrc from "../../assets/images/carAnimation/city.jpeg";
 import carSrc from "../../assets/images/carAnimation/car.png";
+import wheelSrc from "../../assets/images/carAnimation/wheel.png";
 
 export const Header = () => {
   return (
-    <section className="section__padding section__margin">
+    <section className="full-section">
       <div className="header">
-        <img src={citySrc} className="header__city" alt="city" />
-        <img src={carSrc} alt="car" />
+        <div
+          className="header__city"
+          style={{
+            background: `url(${citySrc})`,
+          }}
+        >
+          <div className="header__car">
+            <img src={carSrc} className="header__car-img" alt="car" />
+            <img
+              src={wheelSrc}
+              className="header__wheel-img header__wheel-img-left"
+              alt="wheel_1"
+            />
+            <img
+              src={wheelSrc}
+              className="header__wheel-img header__wheel-img-right"
+              alt="wheel_2"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
