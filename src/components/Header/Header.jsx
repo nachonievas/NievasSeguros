961d3a23e4820e4be46a623280b226a8e5d75e82
NahuelBurdisso <nahuelburdisso@hotmail.com>
2022-07-13 10:10:41 -0300

    Add gradient background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Header.css";
-import citySrc from "../../assets/images/carAnimation/city.jpeg";
 import carSrc from "../../assets/images/carAnimation/car.png";
 import wheelSrc from "../../assets/images/carAnimation/wheel.png";
 
@@ -8,12 +7,7 @@ export const Header = () => {
   return (
     <section className="full-section">
       <div className="header">
-        <div
-          className="header__city"
-          style={{
-            background: `url(${citySrc})`,
-          }}
-        >
+        <div className="header__car-content">
           <div className="header__car">
             <img src={carSrc} className="header__car-img" alt="car" />
             <img
