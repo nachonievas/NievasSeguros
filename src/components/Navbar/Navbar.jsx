25cc42a837d5a822837eca270997ac267e4d0083
Ignacio Nievas <108840985+nachonievas@users.noreply.github.com>
2022-07-16 19:47:09 -0300

    changeLogo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import { useRef } from "react";
 import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../assets/images/logo/logo1.png";
+
 export const Navbar = () => {
   const navRef = useRef();
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
   };
-
   return (
     <header>
-      <h3>logo</h3>
+      <img src={logo} className="navbar_logo" alt="logo_1" />
       <nav ref={navRef}>
         <a href="!#">Home</a>
         <a href="!#">Nosotros</a>
