449066831aa1c34e469ed42352123c3d67017113
NahuelBurdisso <nahuelburdisso@hotmail.com>
2022-07-18 11:17:38 -0300

    Update animations props

--- src/components/PresentationSection/PresentationSection.jsx
+++ src/components/PresentationSection/PresentationSection.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./PresentationSection.css";
 import carSrc from "../../assets/images/carAnimation/car.png";
 import wheelSrc from "../../assets/images/carAnimation/wheel.png";
 import umbrellaSrc from "../../assets/images/carAnimation/umbrella.png";
 
 export const PresentationSection = () => {
+  const carRef = useRef(null);
+  const titleRef = useRef(null);
+
   // Check if the car img and title img are loaded in the DOM, when the component renders
   // if they are, take the x left position of the car img and check if it
   // is equal to the x left position of the title element
   // if so, add the .reveal-left class to the title html element
   // if not, check again in the next frame until the condition is met
 
+  const checkIfRevealLeft = () => {
+    if (carRef.current && titleRef.current) {
+      const carLeft = Math.abs(
+        Math.floor(carRef.current.getBoundingClientRect().left + 50)
+      );
+
+      const titleLeft = Math.abs(
+        Math.floor(titleRef.current.getBoundingClientRect().left)
+      );
+
+      if (titleLeft - carLeft < 3) {
+        titleRef.current.classList.add("reveal-left");
+      } else {
+        requestAnimationFrame(checkIfRevealLeft);
+      }
+    }
+  };
+
+  requestAnimationFrame(checkIfRevealLeft);
+
   return (
     <section className="full-section">
       <div className="presentation-section">
         <div className="presentation-section__car-content">
-          <div className="presentation-section__car">
+          <div ref={carRef} className="presentation-section__car">
             <img
               src={umbrellaSrc}
               className="presentation-section__umbrella-img"
@@ -40,7 +63,7 @@ export const PresentationSection = () => {
           </div>
         </div>
         <div className="presentation-section__title-content">
-          <h1 className="presentation-section__title reveal-left" id="title">
+          <h1 ref={titleRef} className="presentation-section__title" id="title">
             <p>GRUPO</p>
             <p>NIEVAS</p>
           </h1>
