b859a1fb8df77ed4bf5a74fb4185b54187949d92
NahuelBurdisso <nahuelburdisso@hotmail.com>
2022-07-20 17:11:40 -0300

    Add ContactFormSection

---
+++ src/components/ContactFormSection/ContactFormSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ContactFormSection.css";
+import carSrc from "../../assets/images/carAnimation/car.png";
+import wheelSrc from "../../assets/images/carAnimation/wheel.png";
+import umbrellaSrc from "../../assets/images/carAnimation/umbrella.png";
+
+export const ContactFormSection = () => {
+  return (
+    <section className="full-section">
+      <div className="contact-form-section"></div>
+    </section>
+  );
+};
